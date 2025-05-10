ddeaf28d2eeeef4ddcfd11518e65c60cdc15d88e
Nisha <nishubharti98@gmail.com>
2025-05-10 20:33:23 +0530

    class9

---
+++ javacode22.java
@@ -0,0 +1,10 @@
+public class javacode22{
+    public static void phelloworld(){
+        System.out.println("Hello World");
+    }
+
+    public static void main(String args[]){
+      phelloworld(); 
+      phelloworld();
+    }
+}
