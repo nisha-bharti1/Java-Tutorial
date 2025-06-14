ddcbb6d9ded6b9510f870b446ad8a90b8a00e538
Nisha <nishubharti98@gmail.com>
2025-06-14 21:24:10 +0530

    class 14

---
+++ class/javacode36.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Myclass{
+
+    Myclass(){
+        System.out.println("Default constructor is called");
+    }
+
+}
+
+public class javacode36{
+    public static void main(String[] args){
+
+    Myclass obj=new Myclass();
+
+    }
+}
---
+++ class/javacode37.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class ADD{
+
+ADD(){
+    System.out.println(5+6);
+}
+
+ADD(int a, int b){
+    System.out.println(a+b);
+}
+    
+
+}
+
+public class javacode37{
+    public static void main(String[] args){
+
+    ADD obj = new ADD(7,9);
+    // ADD obj1 = new ADD(6,7);
+
+    }
+}
